ec9841d5ca86055da4ccbe680b074620758c8a9a
Leonardo <leonardo.ruma@gmail.com>
2017-07-15 09:28:52 -0300

    editando

--- main.cpp
+++ main.cpp
@@ -7,15 +7,12 @@ using namespace std;
 
 int main(int argc, char **argv){
     VideoCapture stream(0);
-    if (!stream.isOpened()){
-        cout<< "cannot open camera";
-    }
     while (1){
         Mat cameraFrame, frameCanny;
         stream.read(cameraFrame);
         Canny(cameraFrame,frameCanny,50,350,3,false);
         imshow("Original",cameraFrame);
-        imshow("Efeito Canny Edge",frameCanny);
+        imshow("Filtro Canny Edge",frameCanny);
         if(waitKey(10)==27)
             break;
     }
